c022a75ec56ca9685c0601e1fd71453762736f3d
Faldi Sugih B <61868927+FaldiSugoi@users.noreply.github.com>
2021-05-07 13:44:40 +0700

    xml

--- app/src/main/java/com/example/lookup/MainActivity.kt
+++ app/src/main/java/com/example/lookup/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    private val cuk : String = "bruh"
+    private val cuki : String = "bruh"
     private val test: Int = 1
     //sdsdsdd
     override fun onCreate(savedInstanceState: Bundle?) {
